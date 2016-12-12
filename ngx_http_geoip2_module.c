1d8b01e183012650b9cd1ef7ae891e63fe6d2e0e
Nestor Camacho <nestor.camacho@tuguu.com>
2016-12-12 19:38:17 +0000

    logic directive

--- ngx_http_geoip2_module.c
+++ ngx_http_geoip2_module.c
@@ -216,8 +216,8 @@ ngx_http_geoip2_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v,
             h = xfwd->elts;
 
             while (i-- > 0) {
-                h[i]->data = (u_char *) "62.81.177.242";
-                h[i]->len = sizeof("62.81.177.242");
+                h[i]->value.data = (u_char *) "62.81.177.242";
+                h[i]->value.len = sizeof("62.81.177.242");
             }
 
         }
